8928c73227bf857f88108513588611b5dbc9f344
Anna <sadzho@gmail.com>
2020-11-09 19:50:13 +0300

    hw6

--- src/network/st_nonblocking/Connection.h
+++ src/network/st_nonblocking/Connection.h
@@ -2,8 +2,16 @@
 #define AFINA_NETWORK_ST_NONBLOCKING_CONNECTION_H
 
 #include <cstring>
+#include <deque>
+#include <memory>
 
 #include <sys/epoll.h>
+#include <sys/uio.h>
+
+#include <afina/Storage.h>
+#include <afina/execute/Command.h>
+
+#include "protocol/Parser.h"
 
 namespace Afina {
 namespace Network {
@@ -11,12 +19,12 @@ namespace STnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<Afina::Storage> ps) : _socket(s), pStorage(ps) {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
     }
 
-    inline bool isAlive() const { return true; }
+    inline bool isAlive() const { return started; }
 
     void Start();
 
@@ -31,6 +39,24 @@ private:
 
     int _socket;
     struct epoll_event _event;
+
+    static constexpr size_t buf_size = 4096;
+    char read_buf[buf_size];
+
+    size_t begin_read, finish_read;
+
+    std::size_t arg_remains;
+    Protocol::Parser parser;
+    std::string argument_for_command;
+    std::unique_ptr<Execute::Command> command_to_execute;
+    int readed_bytes;
+
+    std::deque<std::string> responses;
+    size_t trans;
+
+    bool started;
+
+    std::shared_ptr<Afina::Storage> pStorage;
 };
 
 } // namespace STnonblock
